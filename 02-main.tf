377923678d1776a07b594c9d87d312e969c86348
KirkAlton-Class7 <232495916+KirkAlton-Class7@users.noreply.github.com>
2026-03-24 22:02:26 -0500

    Updated hash for S3 objects. — 2026-03-24 22:02:06 -0500

--- 02-main.tf
+++ 02-main.tf
@@ -1,31 +1,39 @@
-# S3 Bucket -Frontend
+# ----------------------------------------------------------------
+# S3 Bucket - Frontend
+# ----------------------------------------------------------------
 resource "aws_s3_bucket" "frontend" {
   bucket_prefix = "jenkins-gcheck-bucket-"
   force_destroy = true
-  
 
   tags = {
     Name = "Jenkins G-Check Bucket"
   }
 }
 
-# S3 Object - Repo Links
+# ----------------------------------------------------------------
+# S3 Object - Repo Links (Markdown)
+# ----------------------------------------------------------------
 resource "aws_s3_object" "repo_links" {
   bucket = aws_s3_bucket.frontend.bucket
 
   key    = "repo_links.md"
-  source = "${path.module}/path/to/repo_links.md"
+  source = "${path.module}/s3_objects/repo_links.md"
 
   etag   = filemd5("${path.module}/s3_objects/repo_links.md")
+
+  content_type = "text/markdown"
 }
 
-
-# S3 Object - Repo Validation Screenshot
+# ----------------------------------------------------------------
+# S3 Object - Repo Validation (Image)
+# ----------------------------------------------------------------
 resource "aws_s3_object" "repo_validation" {
   bucket = aws_s3_bucket.frontend.bucket
 
-  key    = "repo_links.md"
-  source = "${path.module}/path/to/repo_validation.png"
+  key    = "repo_validation.png"
+  source = "${path.module}/s3_objects/repo_validation.png"
 
   etag   = filemd5("${path.module}/s3_objects/repo_validation.png")
+
+  content_type = "image/png"
 }
